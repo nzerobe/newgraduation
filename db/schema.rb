b012e9db1db96cae02997f64576bfa72e4b14215
nishrob <nirororo@gmail.com>
2018-06-06 08:32:24 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525153228) do
+ActiveRecord::Schema.define(version: 20180606073200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20180525153228) do
     t.datetime "updated_at", null: false
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
+    t.boolean "admin"
   end
 
   add_foreign_key "comments", "robs"
